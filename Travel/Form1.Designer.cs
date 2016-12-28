a0362417b565e0288f5498413c3ee3d75c95fd2a
ufuk <ufuk-dundar@outlook.com>
2016-12-28 20:14:45 +0300

    Updated

--- Travel/Form1.Designer.cs
+++ Travel/Form1.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPlaces = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstAllPlaces = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.txtNextPlace = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPlaces
+            // 
+            this.txtPlaces.Location = new System.Drawing.Point(125, 42);
+            this.txtPlaces.Multiline = true;
+            this.txtPlaces.Name = "txtPlaces";
+            this.txtPlaces.Size = new System.Drawing.Size(213, 82);
+            this.txtPlaces.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(120, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Places";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(354, 42);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 67);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstAllPlaces
+            // 
+            this.lstAllPlaces.FormattingEnabled = true;
+            this.lstAllPlaces.ItemHeight = 16;
+            this.lstAllPlaces.Location = new System.Drawing.Point(125, 181);
+            this.lstAllPlaces.Name = "lstAllPlaces";
+            this.lstAllPlaces.Size = new System.Drawing.Size(213, 228);
+            this.lstAllPlaces.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(129, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "All Places";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(382, 181);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(263, 37);
+            this.hScrollBar1.TabIndex = 5;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // txtNextPlace
+            // 
+            this.txtNextPlace.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtNextPlace.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNextPlace.Location = new System.Drawing.Point(382, 250);
+            this.txtNextPlace.Multiline = true;
+            this.txtNextPlace.Name = "txtNextPlace";
+            this.txtNextPlace.Size = new System.Drawing.Size(263, 120);
+            this.txtNextPlace.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(382, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Next Place";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(768, 421);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNextPlace);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstAllPlaces);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlaces);
+            this.Name = "Form1";
+            this.Text = "Travel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPlaces;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstAllPlaces;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.TextBox txtNextPlace;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Travel/Form1.cs
+++ Travel/Form1.cs
@@ -16,5 +16,27 @@ namespace Travel
         {
             InitializeComponent();
         }
+        List<string> placeList = new List<string>();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string strPlace = txtPlaces.Text;
+            placeList.Add(strPlace);
+            lstAllPlaces.Items.Add(strPlace);
+            txtPlaces.Text = ""; //txtPlaces.Text=string.Empty;  -- Same thing
+            txtPlaces.Focus();
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (e.NewValue <= placeList.Count && e.NewValue>0)
+            {
+                string newPlace = placeList[e.NewValue-1];
+                txtNextPlace.Text = newPlace;
+            }
+            else
+            {
+                MessageBox.Show("There is No Such Place");
+            }
+        }
     }
 }
